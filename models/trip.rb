876c159a1cf954d6b4af619e3f7d079315883c77
Michael Crockett <mike.crockett76@gmail.com>
2020-01-27 19:58:50 +0000

    Small changes

--- models/trip.rb
+++ models/trip.rb
@@ -9,47 +9,47 @@ class Trip
   attr_reader :id
   attr_accessor :sight_id, :visited
 
-def initialize(options)
-  @id = options['id'].to_i if options['id']
-  @sight_id = options['sight_id'].to_i
-  @visited = options['visited']
-end
+  def initialize(options)
+    @id = options['id'].to_i if options['id']
+    @sight_id = options['sight_id'].to_i
+    @visited = options['visited']
+  end
 
 
 
   def save()
     sql = "INSERT INTO trips
-      (sight_id, visited)
-      VALUES
-      ($1, $2)
-      RETURNING id"
+    (sight_id, visited)
+    VALUES
+    ($1, $2)
+    RETURNING id"
     values = [@sight_id, @visited]
     trip = SqlRunner.run(sql, values).first
     @id = trip['id'].to_i
   end
 
 
-def self.delete_all()
-  sql = "DELETE FROM trips"
-  SqlRunner.run(sql)
-end
+  def self.delete_all()
+    sql = "DELETE FROM trips"
+    SqlRunner.run(sql)
+  end
 
   def self.map_items(trip_data)
     return trip_data.map { |trip| Trip.new(trip) }
   end
 
 
- def self.all()
+  def self.all()
     sql = "SELECT * FROM trips
-          ORDER BY id"
-      list_data = SqlRunner.run(sql)
+    ORDER BY id"
+    list_data = SqlRunner.run(sql)
     trips = self.map_items(list_data)
     return trips
   end
 
   def sight
     sql = "SELECT * from sights
-          WHERE id = $1"
+    WHERE id = $1"
     values = [@sight_id]
     sights_data = SqlRunner.run(sql, values)
     return Sight.new(sights_data[0])
@@ -72,18 +72,29 @@ end
   end
 
 
+  # a method to toggle city's visited status
+  # if the city's attribute is 'visited'
+  # then change to 'not visited'
+  # but, if the city's attribute is 'not visited'
+  # change it to 'visited'
+  # then finish
 
-  def toggle_visited
-    if @visited == 't'
-      @visited = 'f'
-    elsif
-      @visited = 't'
-    end
-    sql = "UPDATE trips
+
+
+
+
+
+    def toggle_visited
+      if @visited == 't'
+        @visited = 'f'
+      elsif
+        @visited = 't'
+      end
+      sql = "UPDATE trips
       SET visited = $1
       WHERE id = $2"
       values = [@visited, @id]
       SqlRunner.run(sql, values)
     end
 
-end #class end
+  end #class end
